f3d8afbcfaf89a0472f027028a1f927ffb654751
cjdango <cjdango@gmail.com>
2019-03-09 23:53:53 +0800

    Playing with layout and add contact page.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
+      <g-link class="nav__link" to="/">
+        <h1 class="brand">cjdango</h1>
+      </g-link>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/contact">Contact</g-link>
       </nav>
+      <hr class="divider">
     </header>
     <slot/>
   </div>
@@ -23,9 +25,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
@@ -37,14 +40,23 @@ body {
 }
 
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  margin-top: 20px;
+  text-align: center;
 }
 
 .nav__link {
   margin-left: 20px;
+}
+
+.brand {
+  display: inline-block;
+  padding: 35px 25px 40px 25px;
+  line-height: 0;
+  background-color: black;
+  color: white;
+}
+
+.divider {
+  margin: 20px 0;
 }
 </style>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h1>Contact page</h1>
+
+    <form netlify>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+      </div>
+
+      <div class="form-group">
+        <label for="mobile">Mobile:</label>
+        <input type="text" name="mobile" id="mobile">
+      </div>
+
+      <div class="form-group">
+        <label for="message">Message:</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+      </div>
+    </form>
+  </Layout>
+</template>
+
+<style>
+.form-group {
+  display: flex;
+  flex-direction: column;
+}
+</style>
